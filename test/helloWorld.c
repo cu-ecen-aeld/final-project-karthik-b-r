c0b3c81b3e15c03afa3eadf195c21d105bb01a70
AnanthD21 <ande9392@colorado.edu>
2022-04-05 17:49:46 +0000

    Test package

---
+++ test/helloWorld.c
@@ -0,0 +1,13 @@
+/*test code to check the complete flow
+ * 1. creating a module
+ * 2. building it as package in buildroot
+ * 3. loading on RaspberryPi to check its working on target
+ *  */
+
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello World\n");
+	return 0;
+}

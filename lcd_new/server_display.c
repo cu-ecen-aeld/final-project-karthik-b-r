4781b1f77c3ef4c9e9efd3560fb10f65bac9c9b6
karthik-b-r <kaba5545@colorado.edu>
2022-04-27 14:38:22 -0600

    code cleaning

--- lcd_new/server_display.c
+++ lcd_new/server_display.c
@@ -12,7 +12,7 @@
 #include <unistd.h>
 #include "wiringPi.h"
 #include "lcd.h"
-//#include "lcd.c"
+
 #define MAX 10000
 #define PORT 8080
 #define SA struct sockaddr
@@ -20,71 +20,46 @@
 
 static int lcdAddr[] = {0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88,0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F,
                        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8 ,0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF,
-	                     0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0X97, 0x98, 0x99, 0x9A, 0x9B, 0x9C ,0x9D, 0x9E, 0x9F,
-		                   0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0xD5, 0xD6, 0xD7, 0xD8, 0xD9, 0xDA, 0xDB, 0xDC ,0xDD, 0xDE, 0xDF};
+	               0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0X97, 0x98, 0x99, 0x9A, 0x9B, 0x9C ,0x9D, 0x9E, 0x9F,
+	               0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0xD5, 0xD6, 0xD7, 0xD8, 0xD9, 0xDA, 0xDB, 0xDC ,0xDD, 0xDE, 0xDF};
 
 
 
 // Function designed for chat between client and server.
-void func(int connfd)
+void readSensorDataFromClient(int connfd)
 {
-	char buff[MAX];
-//	int n;
-	static int line_count=0;
-//        lcd_init();
+  char buff[MAX];
+  lcd_init();
 	// infinite loop for chat
-	for (;;) {
-		bzero(buff, MAX);
-	lcd_init();
-		// read the message from client and copy it in buffer
-	read(connfd, buff, sizeof(buff));
-//        char *p = buff;
-//        int i = 0;
-//        printf("before lcd,  value at p = %c\n", *p);
-//        while (*p != '\0')
-//        {
-//          printChar(*p, lcdAddr[i]);
-//          i++;
-//          p++;
-//          if(63 == i)
-//             i = 0;
-//       }
-//        printf("after lcd:value  = %d\n", i);
-//        sleep(2);
-//	SetCmdMode();
-//	lcd_byte(0x01);
-//	lcd_byte(0x80);
-
-		if (line_count == 0)
-		{
-                       // SetChrMode(); 
-			lcd_print1(buff);
-			line_count=1;
-		}
-		else
-		{
-			lcd_print1(buff);
-			line_count=0;
-		}
-		sleep(1);
-	
-		// print buffer which contains the client contents
-		printf(" %s\t ", buff);
-		bzero(buff, MAX);
-		/*n = 0;
-		// copy server message in the buffer
-		while ((buff[n++] = getchar()) != '\n')
-			;
-
-		// and send that buffer to client
-		write(connfd, buff, sizeof(buff));
-
-		// if msg contains "Exit" then server exit and chat ended.
-		if (strncmp("exit", buff, 4) == 0) {
-			printf("Server Exit...\n");
-			break;
-		}*/
-	}
+  for (;;) 
+  {
+    bzero(buff, MAX);
+    // read the message from client and copy it in buffer
+    read(connfd, buff, sizeof(buff));
+ 
+    //Displays the read buffer on LCD 
+    char *p = buff;
+    int i = 0;
+    //reads reach char and writes it to the specific location on LCD
+    while (*p != '\0')
+    {
+      printChar(*p, lcdAddr[i]);
+      delay(3);
+      i++;
+      p++;
+      if(63 == i)
+        i = 0;
+    }
+    delay(3);
+    SetCmdMode();
+    delay(3);
+    lcd_byte(0x02);   //returns to home posotion for next message ftom client
+    delay(3);
+  
+    // print buffer which contains the client contents
+    printf(" %s\t ", buff);
+    bzero(buff, MAX);
+  }
 }
 
 // Driver function
@@ -135,7 +110,7 @@ int main()
 		printf("server accept the client...\n");
 
 	// Function for chatting between client and server
-	func(connfd);
+	readSensorDataFromClient(connfd);
 
 	// After chatting close the socket
 	close(sockfd);
